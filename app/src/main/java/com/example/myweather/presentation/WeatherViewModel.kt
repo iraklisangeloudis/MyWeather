31781739a97cbe47db6d606a587ea36960a9aa0b
Iraklis Angeloudis <62808163+iraklisangeloudis@users.noreply.github.com>
2024-08-25 00:01:55 +0300

    Moved logic from Main Activity to domain and presentation

--- app/src/main/java/com/example/myweather/presentation/WeatherViewModel.kt
+++ app/src/main/java/com/example/myweather/presentation/WeatherViewModel.kt
@@ -1,4 +1,4 @@
-package com.example.myweather
+package com.example.myweather.presentation
 
 import androidx.lifecycle.LiveData
 import androidx.lifecycle.MutableLiveData
--- app/src/main/java/com/example/myweather/presentation/WeatherViewModelFactory.kt
+++ app/src/main/java/com/example/myweather/presentation/WeatherViewModelFactory.kt
@@ -1,4 +1,4 @@
-package com.example.myweather
+package com.example.myweather.presentation
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
