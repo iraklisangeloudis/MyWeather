1b5fb6e39899b61f546d68a0ed4052085f4b04df
Iraklis Angeloudis <62808163+iraklisangeloudis@users.noreply.github.com>
2024-08-30 12:11:59 +0300

    Refactor getWeatherIcon function

--- app/src/main/java/com/example/myweather/presentation/TemperatureAdapter.kt
+++ app/src/main/java/com/example/myweather/presentation/TemperatureAdapter.kt
@@ -44,33 +44,17 @@ class TemperatureAdapter(private val hourlyData: List<HourlyData>) : RecyclerVie
     private fun getWeatherIcon(weatherCode: Int, isDay:Int): Int {
         return when (weatherCode) {
             0 -> if (isDay==1) R.drawable.clear1 else R.drawable.clear0
-            1 -> if (isDay==1) R.drawable.mc_cloudy1 else R.drawable.mc_cloudy0
-            2 -> if (isDay==1) R.drawable.mc_cloudy1 else R.drawable.mc_cloudy0
-            3 -> if (isDay==1) R.drawable.mc_cloudy1 else R.drawable.mc_cloudy0
-            45 -> R.drawable.fog
-            48 -> R.drawable.fog
-            51 -> R.drawable.mc_cloudy
-            53 -> R.drawable.mc_cloudy
-            55 -> R.drawable.mc_cloudy
-            56 -> R.drawable.mc_cloudy
-            57 -> R.drawable.mc_cloudy
-            61 -> R.drawable.rain
-            63 -> R.drawable.rain
-            65 -> R.drawable.rain
-            66 -> R.drawable.sleet
-            67 -> R.drawable.sleet
-            71 -> R.drawable.l_snow
-            73 -> R.drawable.l_snow
-            75 -> R.drawable.l_snow
+            1, 2, 3 -> if (isDay==1) R.drawable.mc_cloudy1 else R.drawable.mc_cloudy0
+            45, 48 -> R.drawable.fog
+            51, 53, 55, 56, 57 -> R.drawable.mc_cloudy
+            61, 63, 65 -> R.drawable.rain
+            66, 67 -> R.drawable.sleet
+            71, 73, 75 -> R.drawable.l_snow
             77 -> R.drawable.hail
-            80 -> if (isDay==1) R.drawable.shower1 else R.drawable.shower0
-            81 -> if (isDay==1) R.drawable.shower1 else R.drawable.shower0
-            82 -> if (isDay==1) R.drawable.shower1 else R.drawable.shower0
-            85 -> if (isDay==1) R.drawable.l_snow1 else R.drawable.l_snow0
-            86 -> if (isDay==1) R.drawable.l_snow1 else R.drawable.l_snow0
+            80, 81, 82 -> if (isDay==1) R.drawable.shower1 else R.drawable.shower0
+            85, 86 -> if (isDay==1) R.drawable.l_snow1 else R.drawable.l_snow0
             95 -> R.drawable.tstorm
-            96 -> R.drawable.tshower
-            99 -> R.drawable.tshower
+            96, 99 -> R.drawable.tshower
             else -> R.drawable.unknown
         }
     }
