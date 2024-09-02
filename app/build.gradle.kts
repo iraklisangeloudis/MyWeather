424aa39ca4bdab6fb65ca4ae43353fc5b0e3a25e
Iraklis Angeloudis <62808163+iraklisangeloudis@users.noreply.github.com>
2024-09-02 16:36:40 +0300

    include build.gradle.kts in gitignore

---
+++ app/build.gradle.kts
@@ -0,0 +1,83 @@
+import java.util.Properties
+
+plugins {
+    id("com.android.application")
+    id("org.jetbrains.kotlin.android")
+    id("kotlin-kapt")
+}
+
+android {
+    namespace = "com.example.myweather"
+    compileSdk = 34
+
+    defaultConfig {
+        applicationId = "com.example.myweather"
+        minSdkVersion(rootProject.extra["defaultMinSdkVersion"] as Int)
+        targetSdk = 34
+        versionCode = 1
+        versionName = "1.0"
+
+        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
+
+        // Load the custom properties file
+        val properties = Properties()
+        file("../apiKeys.properties").inputStream().use { properties.load(it) }
+        val locationIqApiKey: String? = properties.getProperty("locationIQApiKey") as String?
+        val reverseGeocodeApiKey: String? = properties.getProperty("reverseGeocodeApiKey") as String?
+        buildConfigField("String", "LOCATION_IQ_API_KEY", "\"${locationIqApiKey ?: ""}\"")
+        buildConfigField("String", "REVERSE_GEOCODE_API_KEY", "\"${reverseGeocodeApiKey ?: ""}\"")
+    }
+
+    buildTypes {
+        release {
+            isMinifyEnabled = false
+            proguardFiles(
+                getDefaultProguardFile("proguard-android-optimize.txt"),
+                "proguard-rules.pro"
+            )
+        }
+    }
+    compileOptions {
+        sourceCompatibility = JavaVersion.VERSION_1_8
+        targetCompatibility = JavaVersion.VERSION_1_8
+    }
+    kotlinOptions {
+        jvmTarget = "1.8"
+    }
+    buildFeatures {
+        buildConfig = true
+        viewBinding = true
+    }
+}
+
+dependencies {
+    implementation("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation("com.google.android.gms:play-services-location:21.3.0")
+    implementation("com.google.code.gson:gson:2.8.9")
+    implementation("androidx.recyclerview:recyclerview:1.2.1")
+
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.1")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+
+    // Room components
+    implementation("androidx.room:room-runtime:2.5.2")
+    kapt("androidx.room:room-compiler:2.5.2")
+    implementation("androidx.room:room-ktx:2.5.2")
+
+    // ViewModel
+    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")
+    implementation("androidx.lifecycle:lifecycle-livedata-ktx:2.6.2")
+    // Activity KTX for viewModels()
+    implementation("androidx.activity:activity-ktx:1.8.1")
+
+
+    implementation("androidx.core:core-ktx:1.13.1")
+    implementation("androidx.appcompat:appcompat:1.7.0")
+    implementation("com.google.android.material:material:1.12.0")
+    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    testImplementation("junit:junit:4.13.2")
+    androidTestImplementation("androidx.test.ext:junit:1.2.1")
+    androidTestImplementation("androidx.test.espresso:espresso-core:3.6.1")
+}
